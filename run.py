e36cbb515d176baccf968b1ef0232553ed79ae4d
K-JC <kellytolfts93@hotmail.com>
2023-02-07 19:14:02 +0000

    add random.choice to generate word form list, use print statement to check this works and it does

--- run.py
+++ run.py
@@ -1,3 +1,56 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+"""
+list of words that will be randomly assigned each turn
+by use of import random
+"""
+random_words = [
+    'birthday',
+    'dream',
+    'kitten',
+    'coding',
+    'burger',
+    'castle',
+    'science',
+    'pickle',
+    'magic',
+    'computer',
+    'beach',
+    'holiday',
+    'christmas',
+    'animal',
+    'joker',
+    'lion',
+    'earth',
+    'space',
+    'rocket',
+    'viking',
+    'norway',
+    'sunny',
+    'rain',
+    'snow',
+    'winter',
+    'summer',
+    'autumn',
+    'spring',
+    'thor',
+    'brave',
+    'hero',
+    'giant',
+    'family',
+    'united',
+    'home',
+    'house',
+    'safe']
+
+"""
+select a word at random from above list
+"""
+words = random.choice(random_words).upper()
+# TESTING IT WORKS WILL REMOVE LATER
+print(words)
+print(len(words))
+
+
+
+    
